10e468180cce428bf9af8121aab158c585bd9625
sanfusu <sanfusu@outlook.com>
2022-04-26 20:20:09 +0800

    vscode-server

---
+++ vscode-server/uncompress.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cat e18005f0f1b33c29e81d732535d8c0e47cafb0b5* > e18005f0f1b33c29e81d732535d8c0e47cafb0b5.tar.gz && \
+tar -xavf e18005f0f1b33c29e81d732535d8c0e47cafb0b5.tar.gz && \
+rm -rf e18005f0f1b33c29e81d732535d8c0e47cafb0b5.tar.gz && \
+mv vscode-server-linux-x64 e18005f0f1b33c29e81d732535d8c0e47cafb0b5
